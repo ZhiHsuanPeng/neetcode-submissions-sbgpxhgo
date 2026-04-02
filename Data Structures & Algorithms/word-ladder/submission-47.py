feb96ffadb9569ee07e3c1246d46723a0d6d4102
Jeremy Peng <160497232+ZhiHsuanPeng@users.noreply.github.com>
2026-04-02 21:24:49 +0800

    Add: word-ladder - submission-47

---
+++ Data Structures & Algorithms/word-ladder/submission-47.py
@@ -0,0 +1,32 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        wordSet = set(wordList)
+        if endWord not in wordSet: 
+            return 0 
+
+        beginSet = { beginWord } 
+        endSet = { endWord }  
+        visited = set() 
+
+        res = 1 
+        while beginSet and endSet: 
+            if len(beginSet) > len(endSet): 
+                beginSet, endSet = endSet, beginSet 
+
+            nextSet = set()
+            for word in beginSet: 
+                for i in range(len(word)): 
+                    for c in 'abcdefghijklmnopqrstuvwxyz': 
+                        nextWord = word[:i] + c + word[i + 1:] 
+
+                        if nextWord in endSet: 
+                            return res + 1  
+
+                        if nextWord in wordSet and nextWord not in visited: 
+                            visited.add(nextWord) 
+                            nextSet.add(nextWord) 
+
+            beginSet = nextSet 
+            res += 1 
+
+        return 0
