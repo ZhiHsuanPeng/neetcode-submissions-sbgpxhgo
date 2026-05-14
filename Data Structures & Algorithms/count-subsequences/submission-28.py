f993ee7fb06d49d4ce7d63787571cd7c32092595
Jeremy Peng <160497232+ZhiHsuanPeng@users.noreply.github.com>
2026-05-14 22:30:21 +0800

    Add: count-subsequences - submission-28

---
+++ Data Structures & Algorithms/count-subsequences/submission-28.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        dp = [0] * (len(t) + 1)
+        dp[len(t)] = 1
+
+        for r in range(len(s) - 1, -1, -1):
+
+            prevDiag = 1  # dp[r+1][len(t)]
+
+            for c in range(len(t) - 1, -1, -1):
+
+                temp = dp[c]
+
+                if s[r] == t[c]:
+                    dp[c] = dp[c] + prevDiag
+
+                prevDiag = temp
+
+        return dp[0]
