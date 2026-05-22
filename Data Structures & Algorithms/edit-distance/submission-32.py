c72c35403b03360403f6e2c15a14fa2c39141d94
Jeremy Peng <160497232+ZhiHsuanPeng@users.noreply.github.com>
2026-05-22 20:53:04 +0800

    Add: edit-distance - submission-32

---
+++ Data Structures & Algorithms/edit-distance/submission-32.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        rows = len(word1) 
+        cols = len(word2) 
+
+        dp = [ 0 for _ in range(cols + 1) ]
+        
+        for c in range(cols + 1): 
+            dp[c] = cols - c
+
+        for r in range(rows - 1, -1, -1):  
+            diag = dp[-1] 
+            dp[-1] = rows - r
+            
+            for c in range(cols - 1, -1, -1): 
+                temp = dp[c]
+
+                if word1[r] == word2[c]: 
+                    dp[c] = diag
+                else: 
+                    dp[c] = 1 + min(dp[c], dp[c + 1], diag)  
+
+                diag = temp
+
+        return dp[0]
