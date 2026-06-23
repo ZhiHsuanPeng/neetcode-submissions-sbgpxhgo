4386ec4636b8c1e7d4805f0051d05ddc30bb186c
Jeremy Peng <160497232+ZhiHsuanPeng@users.noreply.github.com>
2026-06-23 21:40:04 +0800

    Add: partition-labels - submission-21

---
+++ Data Structures & Algorithms/partition-labels/submission-21.py
@@ -0,0 +1,20 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last = {} 
+
+        for i in range(len(s)): 
+            cur =  s[i] 
+            last[cur] = i 
+
+        res = [] 
+        
+        curLast = 0 
+        start = 0 
+        for i in range(len(s)): 
+            curLast = max(curLast, last[s[i]]) 
+
+            if i == curLast: 
+                res.append(curLast - start + 1) 
+                start = i + 1  
+        
+        return res
