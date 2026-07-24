ed9efcd23b8bfa1d9425dae44661dcd44eceffc1
Jeremy Peng <160497232+ZhiHsuanPeng@users.noreply.github.com>
2026-07-24 22:00:26 +0800

    Add: jump-game - submission-14

---
+++ Data Structures & Algorithms/jump-game/submission-14.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        curMax = 0 
+
+        for i in range(len(nums)): 
+            if curMax < i: 
+                return False 
+
+            curMax = max(curMax, i + nums[i]) 
+
+            
+
+        return True
